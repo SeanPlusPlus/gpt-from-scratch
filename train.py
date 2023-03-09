98701c3ca5555c1585f1708da6df3682ad6adb71
Sean <seanmichaelstephenson@gmail.com>
2023-03-09 13:10:42 -0800

    main: tensor comments

--- train.py
+++ train.py
@@ -18,4 +18,8 @@ encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
 # encode the entire text dataset and store into a torch.Tensor
+# which essentially converts each character of text into an int
 data = torch.tensor(encode(text), dtype=torch.long)
+
+# get the first 1,000 characters in the text
+# print(data[:1000])
