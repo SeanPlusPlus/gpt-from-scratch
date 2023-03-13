b3088db9af66f4439cdf1b932e19ee72b2ae8bb8
Sean <seanmichaelstephenson@gmail.com>
2023-03-13 10:21:03 -0700

    main: block size

--- train.py
+++ train.py
@@ -22,9 +22,21 @@ decode = lambda l: ''.join([itos[i] for i in l])
 data = torch.tensor(encode(text), dtype=torch.long)
 
 # get the first 1,000 characters in the text
-# print(data[:1000])
+print(data[:6])
 
 # split up data into training set (90%) and validation (last 10%)
 n = int(0.9 * len(data))
 train_data = data[:n]
 val_data = data[n:]
+
+block_size = 8
+train_data[:block_size+1]
+
+print (train_data)
+
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is {context} the target: {target}")
